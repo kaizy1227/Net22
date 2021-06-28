0120a854618c9a77a236c7d9206ecfefefd1ca42
unknown <biminhiabo2@gmail.com>
2021-06-28 17:38:09 +0700

    mailchimp

---
+++ NetCuoiKy/Controllers/MailchimpControllerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MailChimp;
+using MailChimp.Helper;
+using MailChimp.Lists;
+using NetCuoiKy.Models;
+
+namespace NetCuoiKy.Controllers
+{
+    public class MailchimpControllerController : Controller
+    {
+        // GET: MailchimpController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddSubcribeUser(FormCollection frc)
+        {
+            string userEmail = frc["subcribe"];
+            MailChimpManager mc = new MailChimpManager("ce3c1cd09bacdd8da0a4ee4b03636641-us6");
+
+            //	Create the email parameter
+            EmailParameter email = new EmailParameter()
+            {
+                Email = userEmail
+            };
+
+            EmailParameter results = mc.Subscribe("58e8eff755", email);
+            Message msg = new Message()
+            {
+                textMessage = "Thank you :3"
+            };
+
+            return Redirect("/lien-he");
+        }
+    }
+}
---
+++ NetCuoiKy/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetCuoiKy.Models
+{
+    public class Message
+    {
+
+        public string textMessage { get; set; }
+
+    }
+}
